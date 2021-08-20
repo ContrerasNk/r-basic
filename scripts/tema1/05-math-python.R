168d8ab2f26b7eed01b2a3d0a7ddd45c3ca901d6
John Doe <contrerasnetk@gmail.com>
2021-08-20 01:30:50 -0500

    Tarea2 completada

---
+++ scripts/tema1/05-math-python.R
@@ -0,0 +1,231 @@
+## import math
+
+
+## math.pi
+
+
+## math.e
+
+
+## math.tau
+
+
+## tau = 2*math.pi
+
+## tau
+
+
+## math.inf
+
+
+## -math.inf
+
+
+## float('inf')
+
+
+## math.nan
+
+
+## float('nan')
+
+
+## math.sqrt(-1.0)
+
+
+## math.log(0.0)
+
+
+## math.exp(1000.0)
+
+
+## math.pow(math.nan, 0)
+
+
+## math.nan+5
+
+
+## math.hypot(math.nan, math.inf)
+
+
+## 2+2
+
+
+## 3-5
+
+
+## 3*5
+
+
+## 3/5
+
+
+## math.ceil(3.4523)
+
+
+## math.floor(3.4523)
+
+
+## math.trunc(3.4523)
+
+
+## math.copysign(3, -2)
+
+
+## math.copysign(3.0, -0.0)
+
+
+## math.fabs(5)
+
+
+## math.fabs(-5.0)
+
+
+## 2**5
+
+
+## math.factorial(4)
+
+
+## x = 5
+
+## y = 2
+
+## 
+## math.factorial(x) / (math.factorial(y) * math.factorial(x-y))
+
+
+## math.fmod(7,3)
+
+
+## 7%3
+
+
+## 7//3
+
+
+## math.remainder(7, 3)
+
+
+## math.modf(-4.25)
+
+
+## math.gcd(24, 36)
+
+
+## math.isfinite(2.5)
+
+
+## math.isinf(4.6)
+
+
+## math.isnan(4.5)
+
+
+## math.sqrt(2)**2 == 2
+
+
+## math.isclose(math.sqrt(2)**2, 2, rel_tol=1e-09)
+
+
+## math.sqrt(2)**2
+
+
+## math.exp(3)
+
+
+## math.e**3
+
+
+## math.pow(math.e, 3)
+
+
+## math.expm1(1)
+
+
+## math.exp(1) - 1
+
+
+## math.exp(1e-05)-1
+
+
+## math.expm1(1e-05)
+
+
+## math.log(12)
+
+
+## math.log(12, 2)
+
+
+## math.log1p(1e-5)
+
+
+## math.log2(32)
+
+
+## math.log10(1000000)
+
+
+## math.sqrt(64)
+
+
+## math.sin(180)
+
+
+## math.cos(math.pi)
+
+
+## math.tan(math.pi/2)
+
+
+## math.asin(1)
+
+
+## math.acos(1)
+
+
+## math.atan(1)
+
+
+## math.degrees(0.7853981633974483)
+
+
+## math.radians(60)
+
+
+## math.cos(math.radians(60))
+
+
+## math.hypot(3, 4)
+
+
+## math.sqrt(3**2+4**2)
+
+
+## math.degrees(math.atan2(4, 3))
+
+
+## math.sinh(0)
+
+
+## math.cosh(0)
+
+
+## math.tanh(0)
+
+
+## math.erf(0)
+
+
+## math.erf(math.pi)
+
+
+## math.erfc(math.pi)
+
+
+## math.gamma(5.5)
+
+
+## math.lgamma(5)
+
---
+++ scripts/tema1/07-io.R
@@ -0,0 +1,117 @@
+## print("Hola, me llamo Juan Gabriel")
+
+
+## x = 5
+
+## print(x)
+
+
+## print("El valor de la variable x es: "+str(x))
+
+
+## texto = input("Introduce aqu攼㹤 tu nombre: ")
+
+
+## texto
+
+
+## numero = int(input("Introduce aqu攼㹤 tu edad"))
+
+
+## numero
+
+
+## 2*numero + 5
+
+
+## x = 5
+
+
+## print(x)
+
+
+## x = 'j'
+
+
+## print(x)
+
+
+## x = 3.1415
+
+
+## print(x)
+
+
+## x = True
+
+
+## print(x)
+
+
+## x = 'h'
+
+
+## print("La variable x ahora se trata de un car攼㸱cter: "+ x)
+
+
+## x = 5
+
+
+## print("La variable x es entera: "+str(x))
+
+
+## x = "La variable x es ahora una cadena de caracteres (string)"
+
+
+## print(x)
+
+
+## si condicion booleana:
+
+##     c昼㸳digo que se ejecuta si la condici昼㸳n es verdad
+
+##     ..
+
+##     ..
+
+##     ..
+
+## siguiente c昼㸳digo que se ejecuta siempre
+
+
+## x = int(input("Escribe un n昼㹡mero:"))
+
+
+## if x == 5:
+
+##     print("Has escrito el n昼㹡mero 5")
+
+
+## x = int(input("Escribe un n昼㹡mero:"))
+
+## if x < 5:
+
+##     print("Has escrito un n昼㹡mero menor que 5")
+
+## elif x < 10:
+
+##     print("Has escrito un n昼㹡mero menor que 10 y mayor o igual a 5")
+
+## else:
+
+##     print("EL n昼㹡mero que has escrito es mayor o igual que 10")
+
+
+## x = int(input("Escribe un n昼㹡mero:"))
+
+## if x>0 and x < 10:
+
+##     print("Has escrito un n昼㹡mero en el rango de [0,10]")
+
+
+## x = int(input("Escribe un n昼㹡mero:"))
+
+## if x < 0 or x > 10:
+
+##     print("Tu n昼㹡mero escrito no se encuentra dentro del rango [0,10]")
+
